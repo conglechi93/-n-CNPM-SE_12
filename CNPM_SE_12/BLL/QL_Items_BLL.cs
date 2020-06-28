38a83d03fc9611172550f4ddf5ba7a52f8749797
conglechi93 <conglechi93@gmail.com>
2020-06-28 23:16:32 +0700

    Add project files.

---
+++ CNPM_SE_12/BLL/QL_Items_BLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNPM_SE_12.BLL
+{
+    class QL_Items_BLL
+    {
+    }
+}
