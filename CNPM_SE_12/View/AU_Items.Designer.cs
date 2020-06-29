732e3029a0784edec159c742ee068ad726952af3
xuanvinh <xuanvinh@DESKTOP-6CR7ADL>
2020-06-29 18:34:57 +0700

    new2

--- CNPM_SE_12/View/AU_Items.Designer.cs
+++ CNPM_SE_12/View/AU_Items.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.txt_IDItems = new System.Windows.Forms.TextBox();
             this.txt_ItemsName = new System.Windows.Forms.TextBox();
-            this.txt_CtgName = new System.Windows.Forms.TextBox();
             this.txt_Price = new System.Windows.Forms.TextBox();
-            this.txt_State = new System.Windows.Forms.TextBox();
             this.btn_OK = new System.Windows.Forms.Button();
             this.btn_Cancle = new System.Windows.Forms.Button();
             this.lb_Subject = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Number = new System.Windows.Forms.TextBox();
+            this.lb_Number = new System.Windows.Forms.Label();
+            this.cbb_LoaiHang = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // txt_IDItems
@@ -48,10 +48,10 @@
             this.txt_IDItems.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_IDItems.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_IDItems.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txt_IDItems.Location = new System.Drawing.Point(144, 19);
-            this.txt_IDItems.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_IDItems.Location = new System.Drawing.Point(192, 23);
+            this.txt_IDItems.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_IDItems.Name = "txt_IDItems";
-            this.txt_IDItems.Size = new System.Drawing.Size(178, 18);
+            this.txt_IDItems.Size = new System.Drawing.Size(313, 22);
             this.txt_IDItems.TabIndex = 0;
             // 
             // txt_ItemsName
@@ -60,23 +60,11 @@
             this.txt_ItemsName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_ItemsName.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_ItemsName.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txt_ItemsName.Location = new System.Drawing.Point(144, 50);
-            this.txt_ItemsName.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_ItemsName.Location = new System.Drawing.Point(192, 62);
+            this.txt_ItemsName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_ItemsName.Name = "txt_ItemsName";
-            this.txt_ItemsName.Size = new System.Drawing.Size(178, 18);
+            this.txt_ItemsName.Size = new System.Drawing.Size(313, 22);
             this.txt_ItemsName.TabIndex = 1;
-            // 
-            // txt_CtgName
-            // 
-            this.txt_CtgName.BackColor = System.Drawing.Color.LightGray;
-            this.txt_CtgName.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_CtgName.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_CtgName.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txt_CtgName.Location = new System.Drawing.Point(144, 82);
-            this.txt_CtgName.Margin = new System.Windows.Forms.Padding(2);
-            this.txt_CtgName.Name = "txt_CtgName";
-            this.txt_CtgName.Size = new System.Drawing.Size(178, 18);
-            this.txt_CtgName.TabIndex = 2;
             // 
             // txt_Price
             // 
@@ -84,23 +72,11 @@
             this.txt_Price.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_Price.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Price.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txt_Price.Location = new System.Drawing.Point(144, 114);
-            this.txt_Price.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Price.Location = new System.Drawing.Point(192, 140);
+            this.txt_Price.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Price.Name = "txt_Price";
-            this.txt_Price.Size = new System.Drawing.Size(178, 18);
+            this.txt_Price.Size = new System.Drawing.Size(313, 22);
             this.txt_Price.TabIndex = 3;
-            // 
-            // txt_State
-            // 
-            this.txt_State.BackColor = System.Drawing.Color.LightGray;
-            this.txt_State.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_State.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_State.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txt_State.Location = new System.Drawing.Point(144, 146);
-            this.txt_State.Margin = new System.Windows.Forms.Padding(2);
-            this.txt_State.Name = "txt_State";
-            this.txt_State.Size = new System.Drawing.Size(178, 18);
-            this.txt_State.TabIndex = 4;
             // 
             // btn_OK
             // 
@@ -108,10 +84,10 @@
             this.btn_OK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_OK.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_OK.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.btn_OK.Location = new System.Drawing.Point(55, 190);
-            this.btn_OK.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_OK.Location = new System.Drawing.Point(71, 231);
+            this.btn_OK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_OK.Name = "btn_OK";
-            this.btn_OK.Size = new System.Drawing.Size(107, 36);
+            this.btn_OK.Size = new System.Drawing.Size(143, 44);
             this.btn_OK.TabIndex = 5;
             this.btn_OK.Text = "OK";
             this.btn_OK.UseVisualStyleBackColor = false;
@@ -123,22 +99,24 @@
             this.btn_Cancle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Cancle.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Cancle.ForeColor = System.Drawing.Color.White;
-            this.btn_Cancle.Location = new System.Drawing.Point(191, 190);
-            this.btn_Cancle.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Cancle.Location = new System.Drawing.Point(321, 231);
+            this.btn_Cancle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_Cancle.Name = "btn_Cancle";
-            this.btn_Cancle.Size = new System.Drawing.Size(107, 36);
+            this.btn_Cancle.Size = new System.Drawing.Size(143, 44);
             this.btn_Cancle.TabIndex = 6;
             this.btn_Cancle.Text = "Cancle";
             this.btn_Cancle.UseVisualStyleBackColor = false;
+            this.btn_Cancle.Click += new System.EventHandler(this.btn_Cancle_Click);
             // 
             // lb_Subject
             // 
             this.lb_Subject.AutoSize = true;
             this.lb_Subject.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_Subject.ForeColor = System.Drawing.Color.Navy;
-            this.lb_Subject.Location = new System.Drawing.Point(21, 18);
+            this.lb_Subject.Location = new System.Drawing.Point(28, 22);
+            this.lb_Subject.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lb_Subject.Name = "lb_Subject";
-            this.lb_Subject.Size = new System.Drawing.Size(105, 19);
+            this.lb_Subject.Size = new System.Drawing.Size(128, 23);
             this.lb_Subject.TabIndex = 8;
             this.lb_Subject.Text = "Mã sản phẩm:";
             // 
@@ -147,9 +125,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(21, 49);
+            this.label1.Location = new System.Drawing.Point(28, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 19);
+            this.label1.Size = new System.Drawing.Size(133, 23);
             this.label1.TabIndex = 8;
             this.label1.Text = "Tên sản phẩm:";
             // 
@@ -158,52 +137,80 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Navy;
-            this.label2.Location = new System.Drawing.Point(21, 81);
+            this.label2.Location = new System.Drawing.Point(28, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 19);
+            this.label2.Size = new System.Drawing.Size(99, 23);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Mã Category:";
+            this.label2.Text = "Loại hàng:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Navy;
-            this.label3.Location = new System.Drawing.Point(21, 113);
+            this.label3.Location = new System.Drawing.Point(28, 139);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 19);
+            this.label3.Size = new System.Drawing.Size(43, 23);
             this.label3.TabIndex = 8;
             this.label3.Text = "Giá:";
             // 
-            // label4
+            // txt_Number
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Navy;
-            this.label4.Location = new System.Drawing.Point(21, 145);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 19);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Tình trạng:";
+            this.txt_Number.BackColor = System.Drawing.Color.LightGray;
+            this.txt_Number.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Number.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Number.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txt_Number.Location = new System.Drawing.Point(192, 178);
+            this.txt_Number.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Number.Name = "txt_Number";
+            this.txt_Number.Size = new System.Drawing.Size(313, 22);
+            this.txt_Number.TabIndex = 4;
+            // 
+            // lb_Number
+            // 
+            this.lb_Number.AutoSize = true;
+            this.lb_Number.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Number.ForeColor = System.Drawing.Color.Navy;
+            this.lb_Number.Location = new System.Drawing.Point(28, 176);
+            this.lb_Number.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_Number.Name = "lb_Number";
+            this.lb_Number.Size = new System.Drawing.Size(90, 23);
+            this.lb_Number.TabIndex = 8;
+            this.lb_Number.Text = "Số lượng:";
+            // 
+            // cbb_LoaiHang
+            // 
+            this.cbb_LoaiHang.BackColor = System.Drawing.Color.LightGray;
+            this.cbb_LoaiHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbb_LoaiHang.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbb_LoaiHang.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.cbb_LoaiHang.FormattingEnabled = true;
+            this.cbb_LoaiHang.Location = new System.Drawing.Point(192, 99);
+            this.cbb_LoaiHang.Margin = new System.Windows.Forms.Padding(4);
+            this.cbb_LoaiHang.Name = "cbb_LoaiHang";
+            this.cbb_LoaiHang.Size = new System.Drawing.Size(313, 29);
+            this.cbb_LoaiHang.TabIndex = 9;
             // 
             // AU_Items
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 254);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(544, 313);
+            this.Controls.Add(this.cbb_LoaiHang);
+            this.Controls.Add(this.lb_Number);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lb_Subject);
             this.Controls.Add(this.btn_Cancle);
             this.Controls.Add(this.btn_OK);
-            this.Controls.Add(this.txt_State);
+            this.Controls.Add(this.txt_Number);
             this.Controls.Add(this.txt_Price);
-            this.Controls.Add(this.txt_CtgName);
             this.Controls.Add(this.txt_ItemsName);
             this.Controls.Add(this.txt_IDItems);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AU_Items";
             this.Text = "AU_Items";
             this.ResumeLayout(false);
@@ -214,15 +221,15 @@
         #endregion
         private System.Windows.Forms.TextBox txt_IDItems;
         private System.Windows.Forms.TextBox txt_ItemsName;
-        private System.Windows.Forms.TextBox txt_CtgName;
         private System.Windows.Forms.TextBox txt_Price;
-        private System.Windows.Forms.TextBox txt_State;
         private System.Windows.Forms.Button btn_OK;
         private System.Windows.Forms.Button btn_Cancle;
         private System.Windows.Forms.Label lb_Subject;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Number;
+        private System.Windows.Forms.Label lb_Number;
+        private System.Windows.Forms.ComboBox cbb_LoaiHang;
     }
 }
--- CNPM_SE_12/View/AU_Items.cs
+++ CNPM_SE_12/View/AU_Items.cs
@@ -23,7 +23,17 @@ namespace CNPM_SE_12.View
         {
             ID_Items = s;
             InitializeComponent();
+            set_cbbCtg();
             ShowData();
+        }
+        private void set_cbbCtg()
+        {
+            List<Category> ctg = BLL.QL_Items_BLL.Instance.getCtg_BLL();
+            foreach (Category i in ctg)
+            {
+                cbb_LoaiHang.Items.Add(new CBBCtg { Text = i.Category_Name, Values = Convert.ToInt32(i.ID_Category) });
+            }
+
         }
 
         private void ShowData()
@@ -38,16 +48,54 @@ namespace CNPM_SE_12.View
                 Item item = BLL.QL_Items_BLL.Instance.getItems_byID_BLL(ID_Items);
                 txt_IDItems.Text = item.ID_Items;
                 txt_ItemsName.Text = item.Items_Name;
-                txt_CtgName.Text = item.ID_Category;
                 txt_Price.Text = item.Price.ToString();
-                txt_State.Text = item.Status;
+                txt_Number.Text = item.Reserve.ToString();
+                cbb_LoaiHang.SelectedIndex = Convert.ToInt32(item.ID_Category) - 1;
+
+            }
+        }
+        public void AU()
+        {
+            string Name_Item = txt_ItemsName.Text;
+            string Price = txt_Price.Text;
+            string Number = txt_Number.Text;
+            string Category = (cbb_LoaiHang.SelectedIndex + 1).ToString();
+            if (ID_Items == "")
+            {
+                //Category ctg = new Category()
+                //{
+                //    ID_Category = txtCtg_ID.Text,
+                //    Category_Name = txtCtg_Name.Text
+
+                //};
+                //if (BLL.QL_Items_BLL.Instance.Add_Ctg_BLL(ctg))
+                //{
+                //    MessageBox.Show("Add thành công !");
+                //}
+            }
+            else
+            {
+                if (BLL.QL_Items_BLL.Instance.Edit_Items_BLL(ID_Items, Name_Item, Price, Number, Category))
+                {
+                    MessageBox.Show("Edit thành công !");
+                }
 
             }
         }
 
         private void btn_OK_Click(object sender, EventArgs e)
         {
+            AU();
+            if (D != null)
+            {
+                D();
+                this.Close();
+            };
+        }
 
+        private void btn_Cancle_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
