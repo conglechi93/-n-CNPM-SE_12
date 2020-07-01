550f10b5ce7bb6d9e83b4e8caac2273905208aa3
minhhuan0101 <minhhuan.luong0101@gmail.com>
2020-07-01 18:25:38 +0700

    gdh

--- CNPM_SE_12/View/Login.cs
+++ CNPM_SE_12/View/Login.cs
@@ -35,7 +35,7 @@ namespace CNPM_SE_12
 
         public void ReadData()
         {
-            string[] lines = File.ReadAllLines(@"E:\check.txt");
+            string[] lines = File.ReadAllLines(@"K:\check.txt");
 
             if (lines[0] == "true")
             {
@@ -48,7 +48,7 @@ namespace CNPM_SE_12
 
         public void WriteData()
         {
-            String filepath = "E:\\check.txt";
+            String filepath = "K:\\check.txt";
             FileStream fs = new FileStream(filepath, FileMode.Create);
             StreamWriter sWriter = new StreamWriter(fs, Encoding.UTF8);
             if (cb_Rmb.Checked == true)
