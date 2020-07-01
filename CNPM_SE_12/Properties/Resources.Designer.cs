83fa6a475b7053688f74b8713cfccf099be9dc96
minhhuan0101 <minhhuan.luong0101@gmail.com>
2020-07-01 19:06:06 +0700

    abc

--- CNPM_SE_12/Properties/Resources.Designer.cs
+++ CNPM_SE_12/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace CNPM_SE_12.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bullet_magnify {
+            get {
+                object obj = ResourceManager.GetObject("bullet_magnify", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bullet_plus {
             get {
                 object obj = ResourceManager.GetObject("bullet_plus", resourceCulture);
